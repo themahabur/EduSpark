9a66ec680e028dcd991996cb6bff5163b17284cc
mahabur-rahman21 <mahabursoikot04@gmail.com>
2025-07-02 11:31:00 +0600

    Add dark mode toggle functionality to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { use, useContext, useEffect, useState } from "react";
 import { RiArrowDropDownLine } from "react-icons/ri";
 import { Link, NavLink, useNavigate } from "react-router";
 import AuthContext from "../provider/AuthContext";
@@ -9,7 +9,19 @@ const Navbar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme === "true") {
+      setIsDarkMode(true);
+    } else {
+      setIsDarkMode(false);
+    }
+
+    console.log("Dark mode status:", isDarkMode);
+  }, []);
 
   const handleMouse = () => {
     setIsDropdownOpen(!isDropdownOpen);
@@ -114,14 +126,21 @@ const Navbar = () => {
     </>
   );
 
+  const handleThemeToggle = () => {
+    console.log("Theme toggled:", !isDarkMode);
+    localStorage.setItem("theme", !isDarkMode);
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
     <div className="flex justify-between items-center p-4 container mx-auto sticky top-0 z-10 bg-base-100">
       <div className="relative flex justify-center items-center gap-2 md:block">
         <div className="md:hidden ">
-          {
-            isMenuOpen?  <IoClose   size={24} onClick={handleMenuToggle} />:<IoMenu size={24} onClick={handleMenuToggle} />
-          }
-         
+          {isMenuOpen ? (
+            <IoClose size={24} onClick={handleMenuToggle} />
+          ) : (
+            <IoMenu size={24} onClick={handleMenuToggle} />
+          )}
         </div>
         <h1 className="md:text-3xl text-2xl font-medium">
           Edu<span className="text-primary">Spark</span>
@@ -152,24 +171,26 @@ const Navbar = () => {
       <div className="flex justify-center items-center gap-4">
         {user ? (
           <div className="relative cursor-pointer">
-            <div className="rounded-full w-10 h-10 overflow-hidden relative" onClick={() => setIsProfileMenuOpen(!isProfileMenuOpen)}>
+            <div
+              className="rounded-full w-10 h-10 overflow-hidden relative"
+              onClick={() => setIsProfileMenuOpen(!isProfileMenuOpen)}
+            >
               <img referrerPolicy="no-referrer" srcSet={user.photoURL} alt="" />
             </div>
-           {isProfileMenuOpen && (
-          <ul
-            className="absolute top-10 left-0 bg-base-100 border border-gray-100 hidden md:block rounded p-4 space-y-3 z-1"
-            onMouseLeave={() => setIsProfileMenuOpen(false)}
-          >
-            
-            <Link
-              onClick={handleLogout}
-              className="flex  items-center gap-2 hover:text-primary "
-            >
-              <IoArrowRedoCircleOutline size={20} />
-              Logout
-            </Link>
-          </ul>
-        )}
+            {isProfileMenuOpen && (
+              <ul
+                className="absolute top-10 left-0 bg-base-100 border border-gray-100 hidden md:block rounded p-4 space-y-3 z-1"
+                onMouseLeave={() => setIsProfileMenuOpen(false)}
+              >
+                <Link
+                  onClick={handleLogout}
+                  className="flex  items-center gap-2 hover:text-primary "
+                >
+                  <IoArrowRedoCircleOutline size={20} />
+                  Logout
+                </Link>
+              </ul>
+            )}
           </div>
         ) : (
           <div className="flex justify-center items-center gap-4">
@@ -182,49 +203,57 @@ const Navbar = () => {
           </div>
         )}
 
-        <label className="toggle text-base-content">
-          <input type="checkbox" value="dark" className="theme-controller" />
+        <div className="flex items-center">
+          <label className="toggle text-base-content">
+            <input
+              type="checkbox"
+              value="dark"
+              onClick={handleThemeToggle}
+              checked={isDarkMode}
+              className="theme-controller"
+            />
 
-          <svg
-            aria-label="sun"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-          >
-            <g
-              strokeLinejoin="round"
-              strokeLinecap="round"
-              strokeWidth="2"
-              fill="none"
-              stroke="currentColor"
+            <svg
+              aria-label="sun"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
             >
-              <circle cx="12" cy="12" r="4"></circle>
-              <path d="M12 2v2"></path>
-              <path d="M12 20v2"></path>
-              <path d="m4.93 4.93 1.41 1.41"></path>
-              <path d="m17.66 17.66 1.41 1.41"></path>
-              <path d="M2 12h2"></path>
-              <path d="M20 12h2"></path>
-              <path d="m6.34 17.66-1.41 1.41"></path>
-              <path d="m19.07 4.93-1.41 1.41"></path>
-            </g>
-          </svg>
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="12" cy="12" r="4"></circle>
+                <path d="M12 2v2"></path>
+                <path d="M12 20v2"></path>
+                <path d="m4.93 4.93 1.41 1.41"></path>
+                <path d="m17.66 17.66 1.41 1.41"></path>
+                <path d="M2 12h2"></path>
+                <path d="M20 12h2"></path>
+                <path d="m6.34 17.66-1.41 1.41"></path>
+                <path d="m19.07 4.93-1.41 1.41"></path>
+              </g>
+            </svg>
 
-          <svg
-            aria-label="moon"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-          >
-            <g
-              strokeLinejoin="round"
-              strokeLinecap="round"
-              strokeWidth="2"
-              fill="none"
-              stroke="currentColor"
+            <svg
+              aria-label="moon"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
             >
-              <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
-            </g>
-          </svg>
-        </label>
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2"
+                fill="none"
+                stroke="currentColor"
+              >
+                <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
+              </g>
+            </svg>
+          </label>
+        </div>
       </div>
     </div>
   );
